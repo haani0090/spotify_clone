d77f84206aceccc933e1bb2d99bc0642c706d594
haani0090 <haani0090@gmail.com>
2021-12-29 06:46:23 -0800

    finishing

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,30 @@
-import Head from 'next/head'
-import Dashboard from '../components/Dashboard'
-import { useRouter } from 'next/router';
-import { useSession } from 'next-auth/react';
-import Loader from '../components/Loader';
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Dashboard from "../components/Dashboard";
+import Loader from "../components/Loader";
 
 export default function Home() {
-  const router =useRouter();
-  const{status, data:session} = useSession({
-    onUnauthenticated(){
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
       router.push("/auth/signin");
     },
   });
-  if ( status === "loading") {
-    return <Loader/>
+
+  // Loading animation...
+  if (status === "loading") {
+    return <Loader />;
   }
-
-
 
   return (
     <div className="">
       <Head>
-        <title>I_music</title>
+        <title>Spotify - Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Dashboard/>
+      <Dashboard />
     </div>
-  )
+  );
 }
